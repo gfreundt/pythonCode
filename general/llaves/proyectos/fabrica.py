4a00045a04d232a04241179aab4904ab76fdfea8
Gabriel <gfreundt@gmail.com>
2024-11-06 17:15:30 -0500

    fabrica 50%

--- general/llaves/proyectos/fabrica.py
+++ general/llaves/proyectos/fabrica.py
@@ -9,27 +9,15 @@ import os
 def gui(main):
 
     window = ttkb.Toplevel()
-    winx, winy = (1200, 900)
-    x = main.win_posx + 150
-    y = main.win_posy + 150
+    winx, winy = (1500, 1400)
+    x = main.win_posx + 20
+    y = main.win_posy + 20
     window.geometry(f"{winx}x{winy}+{x}+{y}")
     window.title("Fabrica de Proyecto")
     window.iconphoto(False, PhotoImage(file=os.path.join("static", "key1.png")))
 
-    frame_llaves = ttkb.Frame(window)
-    frame_cilindros = ttkb.Frame(window)
-    frame_pines = ttkb.Frame(window)
-    frame_meters = [ttkb.Frame(window) for _ in range(4)]
-    frame_bottom = ttkb.Frame(window)
-
-    frame_llaves.grid(row=0, column=0, columnspan=2, padx=10, pady=5)
-    frame_cilindros.grid(row=1, column=0, columnspan=2, padx=10, pady=5)
-    frame_pines.grid(row=0, column=2, columnspan=2, rowspan=2, padx=10, pady=5)
-    for i in range(4):
-        frame_meters[i].grid(row=2, column=i)
-    frame_bottom.grid(row=3, column=0, columnspan=4, padx=10, pady=5)
-
     data_llaves = {"GGMK": (1, 0), "GMK": (7, 3), "MK": (11, 2), "K": (108, 12)}
+
     data_pines = [
         (23, 11),
         (89, 55),
@@ -45,18 +33,18 @@ def gui(main):
     data_cilindros = (199, 87)
     data_meters = (0.23, 0.65, 0.87, 0.12)
 
-    llaves(frame=frame_llaves, data=data_llaves)
-    pines(frame=frame_pines, pines=data_pines)
-    cilindros(frame=frame_cilindros, cilindros=data_cilindros)
-    meters(frames=frame_meters, data=data_meters)
-
-    # frame input
-    ttkb.Label(frame_bottom, text="Avancemos...").pack()
+    llaves(window, data=data_llaves)
+    pines(window, pines=data_pines)
+    cilindros(window, cilindros=data_cilindros)
+    meters(window, data=data_meters)
+    editar(window)
 
 
-def llaves(frame, data):
+def llaves(window, data):
 
-    # frame llaves
+    frame = ttkb.Frame(window)
+    frame.grid(row=0, column=0, columnspan=2, padx=10, pady=5)
+
     ttkb.Label(frame, text="Avance Creacion de Llaves").grid(
         row=0, column=0, columnspan=5, pady=5
     )
@@ -88,7 +76,11 @@ def llaves(frame, data):
         ttkb.Label(frame, text=str(columna)).grid(column=col, row=6, padx=10)
 
 
-def cilindros(frame, cilindros):
+def cilindros(window, cilindros):
+
+    frame = ttkb.Frame(window)
+    frame.grid(row=1, column=0, columnspan=2, padx=10, pady=5)
+
     # frame llaves
     ttkb.Label(frame, text="Avance Creacion de Cilindros").grid(
         row=0, column=0, columnspan=5, pady=5
@@ -109,7 +101,10 @@ def cilindros(frame, cilindros):
     )
 
 
-def pines(frame, pines):
+def pines(window, pines):
+
+    frame = ttkb.Frame(window)
+    frame.grid(row=0, column=2, columnspan=2, rowspan=2, padx=10, pady=5)
 
     ttkb.Label(frame, text="Uso de Pines").grid(row=0, column=0, columnspan=5)
 
@@ -140,13 +135,49 @@ def pines(frame, pines):
         ttkb.Label(frame, text=str(columna)).grid(column=col, row=12, padx=10)
 
 
-def meters(frames, data):
+def meters(window, data):
 
-    for i, frame in enumerate(frames):
+    frame = ttkb.Frame(window)
+    frame.grid(row=2, column=0, columnspan=4, padx=10, pady=5)
+
+    for i in range(3):
 
         ttkb.Meter(
             frame,
             metersize=180,
             padding=5,
             amountused=data[i] * 100,
-        ).pack()
+        ).grid(row=0, column=i)
+
+
+def editar(window):
+
+    frame_llave = ttkb.Frame(window)
+    frame_llave.grid(row=3, column=0, columnspan=2, padx=10, pady=5)
+    ttkb.Label(frame_llave, text="left").grid(row=0, column=0)
+
+    frame_cil = ttkb.Frame(window)
+    frame_cil.grid(row=3, column=2, columnspan=2, padx=10, pady=5)
+    ttkb.Label(frame_cil, text="right").pack()
+
+    values = ["K-001-002-003", "K-001-002-002", "K-001-002-004"]
+    v = StringVar(value=values[0])
+    ttkb.OptionMenu(frame_llave, v, *values).grid(row=1, column=0)
+
+    codigo = "453984"
+
+    canvas = ttkb.Canvas(frame_llave, width=500, height=200)
+    canvas.grid(row=2, column=0, columnspan=2)
+    FACTOR = 4
+    points_fixed = [10, 30, 0, 20, 10, 10, 70, 10, 90, 0, 110, 20, 90, 30, 70, 30]
+    points_fixed = [70, 30, 90, 30, 110, 20, 90, 0, 70, 10, 10, 10, 0, 20, 10, 30]
+    points_vary = []
+    for x, diente in enumerate(codigo):
+        points_vary.append(10 + (10 * x))
+        points_vary.append(30)
+        points_vary.append(15 + (10 * x))
+        points_vary.append(30 + int(diente))
+
+    canvas.create_polygon(
+        [i * FACTOR for i in points_fixed + points_vary], outline="white", width=3
+    )
